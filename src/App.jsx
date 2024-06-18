54508b00442f1a7cc35b43692305f0714addf82c
Nitya <niittss27@gmail.com>
2024-06-18 09:38:35 +0530

    Completed logic and styling

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ function App() {
     <div className="w-full h-screen bg-gradient-to-r from-indigo-500 to-blue-600">
       <div className="p-4">
         <Faq />
+        <h1 className="m-4 font-semibold">I am below the accordian menu!</h1>
       </div>
     </div>
   );
--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -3,8 +3,19 @@ import Accordian from "./Accordian";
 
 const Faq = () => {
   return (
-    <div className='p-4 bg-gray-200 rounded-lg'>
-      <Accordian />
+    <div className="p-4 bg-gray-200 rounded-lg">
+      <Accordian
+        title="Do you prefer Android or IOS?"
+        answer="I like to use IOS products"
+      />
+      <Accordian
+        title="Do you prefer writing CSS or Tailwind?"
+        answer="I like to use Tailwing"
+      />
+      <Accordian
+        title="Do you prefer Firebase or Superbase?"
+        answer="I like to use Firebase"
+      />
     </div>
   );
 };
