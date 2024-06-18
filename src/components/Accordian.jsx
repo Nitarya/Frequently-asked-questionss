795bdd71e35a90c37854d5a3f4aee9b786ff9873
Nitya <niittss27@gmail.com>
2024-06-18 08:55:16 +0530

    Logic for opening and closing of accordian

--- src/components/Accordian.jsx
+++ src/components/Accordian.jsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useState } from "react";
 
 const Accordian = () => {
-    return (
-        <div className='py-2'>
-            <button className='flex justify-between w-full'>
-<span>This is the title</span>
-<span>+</span>
-            </button>
-          
-            </div>
-    )
-}
+  const [accordianOpen, setAccordianOpen] = useState(false);
 
-export default Accordian
+  return (
+    <div className="py-2">
+      <button className="flex justify-between w-full">
+        <span>This is the title</span>
+        <span>+</span>
+      </button>
+      <div
+        className={`grid overflow-hidden transition-all duration-300 ease-in-out text-slate-600 text-sm ${
+          accordianOpen
+            ? "grid-rows-[1fr] opacity-100"
+            : "grid-rows-[0fr] opacity-0 "
+        } `}
+      >
+        <div>This is the answer</div>
+      </div>
+    </div>
+  );
+};
+
+export default Accordian;
