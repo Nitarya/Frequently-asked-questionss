7c2199880c77ad3c8a79e150868e944d60f5c4af
Nitya <niittss27@gmail.com>
2024-06-18 09:46:08 +0530

    Tailwing classes added

--- src/components/Accordian.jsx
+++ src/components/Accordian.jsx
@@ -10,33 +10,8 @@ const Accordian = ({title, answer}) => {
         className="flex justify-between w-full"
       >
         <span>{title}</span>
-        {/* {accordianOpen ? <span>-</span> : <span>+</span>} */}
-         {/* {accordionOpen ? <span>-</span> : <span>+</span>} */}
-         <svg
-          className="fill-indigo-500 shrink-0 ml-8"
-          width="16"
-          height="16"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <rect
-            y="7"
-            width="16"
-            height="2"
-            rx="1"
-            className={`transform origin-center transition duration-200 ease-out ${
-              accordionOpen && "!rotate-180"
-            }`}
-          />
-          <rect
-            y="7"
-            width="16"
-            height="2"
-            rx="1"
-            className={`transform origin-center rotate-90 transition duration-200 ease-out ${
-              accordionOpen && "!rotate-180"
-            }`}
-          />
-        </svg>
+        {accordionOpen ? <span className="font-extrabold text-lg">-</span> : <span className="font-extrabold text-lg">+</span>}
+         
       </button>
       <div
         className={`grid overflow-hidden transition-all duration-300 ease-in-out text-slate-600 text-sm ${
